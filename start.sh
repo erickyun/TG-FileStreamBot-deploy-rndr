8f5eb97b0186c1bc50d50a02c3e9826ddfcc4cb1
erickyun <71968711+erickyun@users.noreply.github.com>
2024-07-12 19:45:17 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -L "$CONFIG_IN_URL" -o /app/.env; curl -L "$CONFIG_IN_URL" -o .env; python3 -m WebStreamer
+curl -L "$CONFIG_IN_URL" -o /app/.env; curl -L "$CONFIG_IN_URL" -o .env; python3 scheduler.py & python3 -m WebStreamer
